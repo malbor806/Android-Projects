8244ef59bb7d67f5b94caf13ffff3a8dd7096a19
malbor806 <malbor806@gmail.com>
2017-05-24 16:41:17 +0200

    Add ApiManager and Cat class

--- CatsAndJokes/app/src/main/java/com/am/demo/catsandjokes/model/Cat.java
+++ CatsAndJokes/app/src/main/java/com/am/demo/catsandjokes/model/Cat.java
@@ -5,5 +5,22 @@ package com.am.demo.catsandjokes.model;
  */
 
 public class Cat {
+    private String id;
+    private String url;
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
 }
--- CatsAndJokes/app/src/main/java/com/am/demo/catsandjokes/model/CatsGalleryAPI.java
+++ CatsAndJokes/app/src/main/java/com/am/demo/catsandjokes/model/CatsGalleryAPI.java
@@ -1,9 +1,16 @@
 package com.am.demo.catsandjokes.model;
 
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
 /**
  * Created by malbor806 on 24.05.2017.
  */
 
 public interface CatsGalleryAPI {
-    @GET()
+    @GET("images/get?format=xml&results_per_page={pictureNumber}")
+    Call<List<Cat>> getCatsList(@Path("pictureNumber") String pictureNumber);
 }
