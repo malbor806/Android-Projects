df42fb0e3e134ec31e3dd25400c06966936cea47
malbor806 <malbor806@gmail.com>
2017-05-28 14:14:05 +0200

    add draw paths on map

--- MapApp/app/src/main/java/com/am/demo/mapapp/MainActivity.java
+++ MapApp/app/src/main/java/com/am/demo/mapapp/MainActivity.java
@@ -9,9 +9,9 @@ import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.ImageButton;
 
 import com.am.demo.mapapp.model.City;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -19,6 +19,8 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 import java.util.Arrays;
 import java.util.List;
@@ -26,25 +28,39 @@ import java.util.List;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMapClickListener {
     @BindView(R.id.actv_searchCity)
     AutoCompleteTextView searchCityAutoCompleteTextView;
+    @BindView(R.id.ib_drawLines)
+    ImageButton drawTourImageButton;
     private GoogleMap googleMap;
     private List<City> cities;
     private String[] citiesName;
+    private boolean userWantToDrawPath;
+    private PolylineOptions pathToDraw;
+    private Polyline polyline;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        userWantToDrawPath = false;
         ButterKnife.bind(this);
+        setMapFragment();
+        convertJsonFileToCitiesList();
+        addSuggestionListToAutoCompleteTextView();
+        setListeners();
+    }
+
+    private void convertJsonFileToCitiesList() {
+        JSONConventer jsonConventer = new JSONConventer(getResources().openRawResource(R.raw.miasta));
+        cities = jsonConventer.getCitiesList();
+    }
+
+    private void setMapFragment() {
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-        JSONConventer jsonConventer = new JSONConventer(getResources().openRawResource(R.raw.miasta));
-        cities = jsonConventer.getCitiesList();
-        addSuggestionListToAutoCompleteTextView();
-        setListeners();
     }
 
     private void addSuggestionListToAutoCompleteTextView() {
@@ -70,11 +86,27 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             City city = cities.get(index);
             MainActivity.this.setLocation(city.getLatitude(), city.getLongitude());
         });
+        drawTourImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (userWantToDrawPath) {
+                    userWantToDrawPath = false;
+                    if (polyline != null) {
+                        polyline.remove();
+                    }
+                } else {
+                    pathToDraw = new PolylineOptions();
+                    userWantToDrawPath = true;
+                }
+
+            }
+        });
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         this.googleMap = googleMap;
+        googleMap.setOnMapClickListener(this);
         findStartedLocation();
     }
 
@@ -119,4 +151,15 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(latitude, longitude), 11.0f));
     }
 
+    @Override
+    public void onMapClick(LatLng latLng) {
+        if (userWantToDrawPath) {
+            pathToDraw.add(latLng);
+            setLocation(latLng.latitude, latLng.longitude);
+            if (polyline != null) {
+                polyline.remove();
+            }
+            polyline = googleMap.addPolyline(pathToDraw);
+        }
+    }
 }
